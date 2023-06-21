531b36b6c9cead5626468053d030e02566347fd4
David Daniel <davidbd7@gmail.com>
2023-06-21 13:12:04 -0700

    first commit, set up navbar and project structure

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+
+import {NavBar, Welcome, Footer, Services, Transactions} from './components';
+
+function App ()  {
+  return (
+    <div className="min-h-screen">
+      <div className = "gradient-bg-welcome">
+        <NavBar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />        
+    </div>
+  );
+}
+
+export default App;
+
+
